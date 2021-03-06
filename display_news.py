82f27fe913874c008a1f3bf03225d2a3954fae25
nick clawson <nclawson1@hotmail.com>
2021-03-06 14:31:52 -0500

    nw

--- display_news.py
+++ display_news.py
@@ -5,9 +5,9 @@ from flask import Flask, request, render_template, redirect, url_for
 
 project_root = os.path.dirname(os.path.realpath('__file__'))
 # print(os.path.realpath('__file__'))
-template_path = os.path.join(project_root, 'Templates')
-# static_path = os.path.join(project_root, 'static')
-app = Flask(__name__,template_folder=template_path)
+template_path = os.path.join(project_root, '../trulinews.com/Templates')
+static_path = os.path.join(project_root, '../trulinews.com/static')
+app = Flask(__name__,template_folder=template_path,static_folder=static_path)
 
 """
 main_image
