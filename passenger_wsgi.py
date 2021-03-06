63f5015040f693bf839089ae6b7b7b7bdc146024
nick clawson <nclawson1@hotmail.com>
2021-03-05 23:55:18 -0500

    w2

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -3,13 +3,13 @@ import os
 from flask import Flask, request, render_template, redirect, url_for
 
 project_root = os.path.dirname(os.path.realpath('__file__'))
-template_path = os.path.join(project_root, 'app/templates')
+# template_path = os.path.join(project_root, 'app/templates')
 static_path = os.path.join(project_root, 'app/static')
-app = Flask(__name__, template_folder=template_path, static_folder=static_path)
+app = Flask(__name__, template_folder="Templates", static_folder=static_path)
 
 @app.route('/')
 def home():
-    return render_template("index.html",template_folder='Templates')
+    return render_template("index.html")
 
 
 application = app
