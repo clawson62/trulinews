740e2324722e5b5d030090069df5bf2daaa159ee
nick clawson <nclawson1@hotmail.com>
2021-03-06 01:33:06 -0500

    w3

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,17 +1,16 @@
 import os
-import sys
-sys.path.insert(0,'/home/furnixsp/home/trulinews/Templates/')
+
 
 from flask import Flask, request, render_template, redirect, url_for
 
 project_root = os.path.dirname(os.path.realpath('__file__'))
 template_path = os.path.join(project_root, 'Templates')
-static_path = os.path.join(project_root, 'Templates')
+static_path = os.path.join(project_root, 'static')
 app = Flask(__name__, template_folder=template_path, static_folder=static_path)
 
 @app.route('/')
 def home():
-    return render_template("Home.html", **locals())
+    return render_template("Home.html")
 
 
 application = app
